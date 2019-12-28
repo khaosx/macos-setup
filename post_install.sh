ca6b626021e4bc98392d4fa85d9614c820289a43
Kristopher Newman <misterk71@gmail.com>
2019-12-27 19:45:50 -0500

    Disabled MAS check - error

--- post_install.sh
+++ post_install.sh
@@ -32,13 +32,13 @@ printf "************************************************************************
 
 printf "Before we get started, let's get some info about your setup.\\n"
 
-printf "Have you signed in to the Mac App Store? (y/n)\\n"
-read is-signed-in
-echo
-if [[ ! "$is-signed-in" =~ ^[Yy]$ ]]; then
-  printf "Please sign into the Mac App Store\\n"
-  exit 1
-fi
+#printf "Have you signed in to the Mac App Store? (y/n)\\n"
+#read is-signed-in
+#echo
+#if [[ ! "$is-signed-in" =~ ^[Yy]$ ]]; then
+ # printf "Please sign into the Mac App Store\\n"
+ # exit 1
+#fi
 
 # Get system name
 export DEFAULT_COMPUTER_NAME="Lithium"
